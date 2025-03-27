d224f24dc3d6a71757b155b73e6f4ffa915bc99d
Lio <liobensignor@gmail.com>
2025-03-27 12:57:43 -0300

    Segunda parte text 3D

--- src/text-3d.js
+++ src/text-3d.js
@@ -17,13 +17,15 @@ const canvas = document.querySelector('canvas.webgl')
 const scene = new THREE.Scene()
 
 // Axes helper
-const axesHelper = new THREE.AxesHelper()
-scene.add(axesHelper)
+// const axesHelper = new THREE.AxesHelper()
+// scene.add(axesHelper)
 
 /**
  * Textures
  */
 const textureLoader = new THREE.TextureLoader()
+const matcapTexture = textureLoader.load('/textures/matcaps/8.png')
+matcapTexture.colorSpace = THREE.SRGBColorSpace
 
 // Fonts
 const fontLoader = new FontLoader()
@@ -52,10 +54,27 @@ fontLoader.load(
         //     (-textGeometry.boundingBox.max.z - 0.03) * 0.5
         // )
         textGeometry.center()
-        const textMaterial = new THREE.MeshBasicMaterial()
-        textMaterial.wireframe = true
-        const text = new THREE.Mesh(textGeometry, textMaterial)
+        const material = new THREE.MeshMatcapMaterial({ matcap: matcapTexture })
+        const text = new THREE.Mesh(textGeometry, material)
         scene.add(text)
+        
+        const donutGeometry = new THREE.TorusGeometry(0.3, 0.2, 20, 45)
+
+        for (let i = 0; i < 1000; i++) {
+            const donut = new THREE.Mesh(donutGeometry, material)
+            donut.position.x = (Math.random() - 0.5) * 30
+            donut.position.y = (Math.random() - 0.5) * 30
+            donut.position.z = (Math.random() - 0.5) * 30
+
+            donut.rotation.x = Math.random() * Math.PI
+            donut.rotation.y = Math.random() * Math.PI
+
+            const scale = Math.random()
+            donut.scale.set(scale, scale, scale)
+
+            scene.add(donut)
+        }
+
     }
 )
 
